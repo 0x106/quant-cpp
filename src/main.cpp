922e5f200a9ef3a298b077523473a8f66744e670
0x106 <jordan.evan.campbell@gmail.com>
2020-02-29 19:03:32 +0000

    switch to std::vector version

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include "./matrix.h"
 
+using Vector = Quant::Vector<Quant::Format>;
+
 int main (int argc, char** argv) {
     std::cout << "Quant" << std::endl;
 
-    Quant::Vector a {1, 2, 3} ;
+    // Quant::Vector<Quant::Format> a({1, 2, 3});
+    Vector b;
+    Vector c(1);
+    Vector d({1.0f, 2.0f, 3.0f});
 
     return 0;
 }
--- src/matrix.h
+++ src/matrix.h
@@ -1,3 +1,6 @@
+// https://codereview.stackexchange.com/questions/63970/custom-mathematical-vector-class
+
+#include <vector>
 #include <iostream>
 #include <algorithm>
 #include <initializer_list>
@@ -6,75 +9,23 @@ namespace Quant {
 
     using Format = float;
 
+    template<class T>
     class Vector {
         private:
-            Format* m_data;
-            int m_N;
+            std::vector<T> m_data;
 
         public:
-            Vector() : m_data(nullptr), m_N(0) {}
-            virtual ~Vector() {
-                delete m_data;
-                m_data = nullptr;
-                m_N = 0;
-            }
-
-            Vector(int N) {
-                m_N = N;
-                m_data = new Format[m_N];
-            }
-            
-            Vector(int N, Format value) {
-                m_N = N;
-                m_data = new Format[m_N];
-                for (int i = 0; i < m_N; ++i)
-                    m_data[i] = value;
-            }
-
-            Vector(std::initializer_list<Format> data) {
-                m_N = data.size();
-                m_data = new Format[m_N];
+            Vector() {};
+            Vector(size_t n) : m_data(n) {}
+            Vector(const std::vector<T>& v) : m_data(v) {}
+            Vector(std::initializer_list<T> data) {
                 m_data = data;
-                // for( int i = 0; i < m_N; ++i ) { m_data[i] = data[i]; }
             }
 
-            Vector(const Vector& v) : m_data(v.m_data), m_N(v.m_N) {}
+            Vector(const Vector<T>& v) : m_data(v.data()) {}
 
-            // Vector(Vector&& v) : m_data(nullptr), m_N(0) {
-            //     *this = std::move(v);
-            // }
-
-            void print() const;
-
-            // element-wise addition and subtraction
-            Vector& operator+(const Vector& rhs) const;
-            Vector& operator-(const Vector& rhs) const;
-            
-            // dot product
-            Format operator*(const Vector& rhs) const;
-
-            Format l1Norm() const;
-            Format l2Norm() const;
-            Format lpNorm(const int p) const;
-            Format lInfinityNorm() const;
+            std::vector<T> data() const { 
+                return m_data; 
+            }
     };
-
-    // class Matrix {
-    //     private:
-    //         std::vector<Quant::Vector> m_data;
-
-    //     public:
-    //         Matrix();
-    //         virtual ~Matrix();
-            
-    //         Matrix(Format value, int N);
-    //         Matrix(const Quant::Vector& data, int N);
-    //         Matrix(const std::vector<Quant::Vector> data);
-    //         Matrix(const Matrix& m);
-
-    //         void print() const;
-    //         Matrix& operator* (const Matrix& rhs);
-    // };
-
-
 }
